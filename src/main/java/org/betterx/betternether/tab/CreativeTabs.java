8871dfbf06914317114c9f7fc0d8f566c31422fe
Frank <developer@ambertation.de>
2022-12-03 14:01:00 +0100

    Migrated CreativeTabs

--- src/main/java/org/betterx/betternether/tab/CreativeTabs.java
+++ src/main/java/org/betterx/betternether/tab/CreativeTabs.java
@@ -4,7 +4,6 @@ import org.betterx.betternether.BetterNether;
 import org.betterx.betternether.registry.NetherBlocks;
 import org.betterx.betternether.registry.NetherItems;
 
-import net.minecraft.world.flag.FeatureFlagSet;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 
@@ -17,36 +16,26 @@ public class CreativeTabs {
     public static final CreativeModeTab TAB_ITEMS;
 
     static {
-        BN_TAB = new FabricItemGroup(BetterNether.makeID("blocks")) {
-            @Override
-            public ItemStack makeIcon() {
-                return new ItemStack(NetherBlocks.NETHER_GRASS);
-            }
+        BN_TAB = FabricItemGroup
+                .builder(BetterNether.makeID("blocks"))
+                .icon(() -> new ItemStack(NetherBlocks.NETHER_GRASS))
+                .displayItems((featureFlagSet, output, bl) -> {
+                    List<ItemStack> stacks = NetherBlocks.getModBlockItems()
+                                                         .stream()
+                                                         .map(ItemStack::new)
+                                                         .toList();
+                    output.acceptAll(stacks);
+                }).build();
 
-            @Override
-            protected void generateDisplayItems(FeatureFlagSet featureFlagSet, Output output) {
-                List<ItemStack> stacks = NetherBlocks.getModBlockItems()
-                                                     .stream()
-                                                     .map(ItemStack::new)
-                                                     .toList();
-                output.acceptAll(stacks);
-            }
-        };
-
-        TAB_ITEMS = new FabricItemGroup(BetterNether.makeID("items")) {
-            @Override
-            public ItemStack makeIcon() {
-                return new ItemStack(NetherItems.BLACK_APPLE);
-            }
-
-            @Override
-            protected void generateDisplayItems(FeatureFlagSet featureFlagSet, Output output) {
-                List<ItemStack> stacks = NetherItems.getModItems()
-                                                    .stream()
-                                                    .map(ItemStack::new)
-                                                    .toList();
-                output.acceptAll(stacks);
-            }
-        };
+        TAB_ITEMS = FabricItemGroup
+                .builder(BetterNether.makeID("items"))
+                .icon(() -> new ItemStack(NetherItems.BLACK_APPLE))
+                .displayItems((featureFlagSet, output, bl) -> {
+                    List<ItemStack> stacks = NetherItems.getModItems()
+                                                        .stream()
+                                                        .map(ItemStack::new)
+                                                        .toList();
+                    output.acceptAll(stacks);
+                }).build();
     }
 }

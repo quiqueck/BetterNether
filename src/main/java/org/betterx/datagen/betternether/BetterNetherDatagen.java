e0e237d72c27a56cb51e788da6cdb3003d08b2a3
Frank <developer@ambertation.de>
2022-12-10 13:40:26 +0100

    [Changes] Removed redundant Files

--- src/main/java/org/betterx/datagen/betternether/BetterNetherDatagen.java
+++ src/main/java/org/betterx/datagen/betternether/BetterNetherDatagen.java
@@ -4,7 +4,6 @@ import org.betterx.bclib.api.v2.levelgen.biomes.BCLBiomeRegistry;
 import org.betterx.datagen.betternether.worldgen.NetherAdvancementDataProvider;
 import org.betterx.datagen.betternether.worldgen.NetherBiomesDataProvider;
 import org.betterx.datagen.betternether.worldgen.NetherRegistriesDataProvider;
-import org.betterx.datagen.betternether.worldgen.WorldgenRegistriesDataProvider;
 
 import net.minecraft.core.RegistrySetBuilder;
 import net.minecraft.core.registries.Registries;
@@ -19,7 +18,6 @@ public class BetterNetherDatagen implements DataGeneratorEntrypoint {
         NetherBiomesDataProvider.ensureStaticallyLoaded();
 
         final FabricDataGenerator.Pack pack = dataGenerator.createPack();
-        pack.addProvider(WorldgenRegistriesDataProvider::new);
         pack.addProvider(NetherBiomesDataProvider::new);
         pack.addProvider(NetherRegistriesDataProvider::new);
         pack.addProvider(NetherAdvancementDataProvider::new);

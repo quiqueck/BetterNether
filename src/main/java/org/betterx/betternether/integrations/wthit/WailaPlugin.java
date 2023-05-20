3b311d17476007d6dae5892e8f2df5bbe24a09fd
Frank <developer@ambertation.de>
2023-05-21 00:01:52 +0200

    [Change] Disabled EMI and ModMenu Integration for now

--- src/main/java/org/betterx/betternether/integrations/wthit/WailaPlugin.java
+++ src/main/java/org/betterx/betternether/integrations/wthit/WailaPlugin.java
@@ -1,23 +1,23 @@
 package org.betterx.betternether.integrations.wthit;
 
-import org.betterx.betternether.BetterNether;
-import org.betterx.betternether.blocks.BlockCommonPlant;
-
-import net.minecraft.resources.ResourceLocation;
-
-import mcp.mobius.waila.api.IRegistrar;
-import mcp.mobius.waila.api.IWailaPlugin;
-import mcp.mobius.waila.api.TooltipPosition;
-
-public class WailaPlugin implements IWailaPlugin {
-    static class Options {
-        public static final ResourceLocation CROP_PROGRESS = new ResourceLocation("crop_progress");
-    }
-
-    @Override
-    public void register(IRegistrar registrar) {
-        BetterNether.LOGGER.info("Registering Waila-/Wthit-Plugin.");
-
-        registrar.addComponent(NetherPlantProvider.INSTANCE, TooltipPosition.BODY, BlockCommonPlant.class);
-    }
-}
+//import org.betterx.betternether.BetterNether;
+//import org.betterx.betternether.blocks.BlockCommonPlant;
+//
+//import net.minecraft.resources.ResourceLocation;
+//
+//import mcp.mobius.waila.api.IRegistrar;
+//import mcp.mobius.waila.api.IWailaPlugin;
+//import mcp.mobius.waila.api.TooltipPosition;
+//
+//public class WailaPlugin implements IWailaPlugin {
+//    static class Options {
+//        public static final ResourceLocation CROP_PROGRESS = new ResourceLocation("crop_progress");
+//    }
+//
+//    @Override
+//    public void register(IRegistrar registrar) {
+//        BetterNether.LOGGER.info("Registering Waila-/Wthit-Plugin.");
+//
+//        registrar.addComponent(NetherPlantProvider.INSTANCE, TooltipPosition.BODY, BlockCommonPlant.class);
+//    }
+//}

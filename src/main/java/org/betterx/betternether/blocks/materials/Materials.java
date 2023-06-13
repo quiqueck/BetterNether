b89c1a02a0b2da9ce1b76d05f6e86bb119b2f362
Frank <developer@ambertation.de>
2023-06-13 17:02:11 +0200

    [Change] Updated use of BehaviourBuilders to latest BCLib

--- src/main/java/org/betterx/betternether/blocks/materials/Materials.java
+++ src/main/java/org/betterx/betternether/blocks/materials/Materials.java
@@ -7,16 +7,17 @@ import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.MapColor;
 
 public class Materials {
-    public static final BlockBehaviour.Properties NETHER_GRASS = BehaviourBuilders.createGrass(MapColor.GRASS, false);
-    public static final BlockBehaviour.Properties NETHER_SAPLING = BehaviourBuilders
-            .createTickingPlant()
-            .sound(SoundType.CROP)
-            .noOcclusion();
+    public static final BlockBehaviour.Properties NETHER_GRASS = BehaviourBuilders.createGrass(MapColor.GRASS);
+    public static final BlockBehaviour.Properties NETHER_SAPLING = BehaviourBuilders.createPlant().randomTicks()
+                                                                                    .sound(SoundType.CROP)
+                                                                                    .noCollission()
+                                                                                    .noOcclusion();
 
     public static final BlockBehaviour.Properties NETHER_PLANT = BehaviourBuilders
             .createPlant()
             .sound(SoundType.CROP)
-            .noOcclusion();
+            .noOcclusion()
+            .noCollission();
 
     public static BlockBehaviour.Properties makeNetherWood(MapColor color) {
         return BehaviourBuilders.createWood(color, false)
@@ -25,8 +26,7 @@ public class Materials {
     }
 
     public static BlockBehaviour.Properties makeNetherGrass(MapColor color) {
-        return BehaviourBuilders
-                .applyBasePlantSettings(BehaviourBuilders.createGrass(color, false), 0)
-                .isValidSpawn((state, world, pos, type) -> true);
+        return BehaviourBuilders.createGrass(color)
+                                .isValidSpawn((state, world, pos, type) -> true);
     }
 }

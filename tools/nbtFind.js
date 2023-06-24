b968c867e0691cc8f79bac3d7c7c196c64d36f7f
Frank <developer@ambertation.de>
2023-06-24 15:09:31 +0200

    Write compressed

--- tools/nbtFind.js
+++ tools/nbtFind.js
@@ -1,5 +1,6 @@
 const fs = require('fs/promises')
 const nbt = require('prismarine-nbt')
+const zlib = require('zlib')
 
 async function processFile(file) {
     console.log('\n----- Processing file', file)
@@ -15,42 +16,52 @@ async function processFile(file) {
     
     //console.log(storageBlocks)
     let changes = false;
-   blocks
-        .map(element => {return { nbt: element.nbt, src:element, storage:storageBlocks.find(e => e.idx == element.state.value)};})
-        .filter(element => element.storage!==undefined)
-        .forEach(element => {
+    let pos =0;
+    for (element of blocks){
+        const storage = storageBlocks.find(e => e.idx == element.state.value);
+        const nbt = element.nbt;
+        if (storage!==undefined){
             let table = "NONE"
-            if (element.nbt!==undefined && element.nbt.value!==undefined && element.nbt.value.LootTable!==undefined)
-                table = element.nbt.value.LootTable.value
+            if (nbt!==undefined &&nbt.value!==undefined && nbt.value.LootTable!==undefined)
+                table = nbt.value.LootTable.value
 
             if (table === "NONE"){
                 changes = true
                 //randomly pick one of two strings weighted
                 table = Math.random() < 0.3 ? 'betternether:chests/city_surprise' : 'betternether:chests/city_common'                
-                element.src.nbt.value.LootTable = { type: 'string', value: table }                                    
-                delete element.src.nbt.value.Items
+                nbt.value.LootTable = { type: 'string', value: table }                                    
+                delete nbt.value.Items
                 table = "[NEW] " + table
             }
-            console.log(element.storage.name, '->', table)
-        });   
-        
-        if (changes){
-            console.log("WRITING")
-            const nbtData = nbt.writeUncompressed(parsed, type)
-
-            let fl
-            try{
-                fl = await fs.open(file, 'w');
-                fl.createWriteStream().write(nbtData)
-             } finally {
-                await fl?.close();
-             }
-            
+            console.log(storage.name, '->', table, `(${pos})`)
         }
+        pos++;
+    }
+        
+    if (changes){
+        console.log("WRITING")
+        const nbtData = nbt.writeUncompressed(parsed, type)            
+
+        const nbtDataCompressed = await new Promise((resolve, reject) => {
+            zlib.gzip(nbtData, (error, compressed) => {
+                if (error) reject(error)
+                else resolve(compressed)
+            })
+        })
+        let fl
+        try{
+            fl = await fs.open(file, 'w');
+            fl.createWriteStream().write(nbtDataCompressed)
+        } finally {
+            await fl?.close();
+        }
+
+    }
   }
+//compress a buffer with gzip
 
   
-// recursivley gets all files in the directory passed as a parameter and calls processFile for each file
+
 async function main(dir) {
     const files = await fs.readdir(dir)
     for (const file of files) {

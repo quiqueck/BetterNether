c662d2493e32a7bbf326fdcf43e2e45060268dab
Frank <developer@ambertation.de>
2023-07-11 23:21:26 +0200

    [Fix] Remove occlusion shape for largest variant of Ink Bush

--- src/main/java/org/betterx/betternether/blocks/BlockInkBush.java
+++ src/main/java/org/betterx/betternether/blocks/BlockInkBush.java
@@ -7,8 +7,11 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.MaterialColor;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
@@ -16,6 +19,16 @@ import net.fabricmc.api.Environment;
 public class BlockInkBush extends BlockCommonPlant implements SurvivesOnNetherGround {
     public BlockInkBush() {
         super(MaterialColor.COLOR_BLACK);
+    }
+
+    @Override
+    public VoxelShape getOcclusionShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos) {
+        if (blockState.getValue(AGE) == 3) {
+            //return an empty shape to prevent occlusion
+            return Shapes.empty();
+        }
+
+        return super.getOcclusionShape(blockState, blockGetter, blockPos);
     }
 
     @Override

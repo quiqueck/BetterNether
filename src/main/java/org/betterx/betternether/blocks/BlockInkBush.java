2a128cd6d4ccee34ea9c3bb3a39cd1fdcae4265c
Frank <developer@ambertation.de>
2023-07-11 23:21:26 +0200

    [Fix] Remove occlusion shape for largest variant of Ink Bush

--- src/main/java/org/betterx/betternether/blocks/BlockInkBush.java
+++ src/main/java/org/betterx/betternether/blocks/BlockInkBush.java
@@ -1,5 +1,6 @@
 package org.betterx.betternether.blocks;
 
+import org.betterx.bclib.behaviours.BehaviourBuilders;
 import org.betterx.bclib.behaviours.interfaces.BehaviourPlant;
 import org.betterx.betternether.interfaces.SurvivesOnNetherGround;
 import org.betterx.betternether.registry.NetherBlocks;
@@ -8,15 +9,31 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 
 public class BlockInkBush extends BlockCommonPlant implements SurvivesOnNetherGround, BehaviourPlant {
     public BlockInkBush() {
-        super(MapColor.COLOR_BLACK);
+        super(BehaviourBuilders.createPlant(MapColor.COLOR_BLACK)
+                               .sound(SoundType.CROP)
+                               .noOcclusion()
+        );
+    }
+
+    @Override
+    public VoxelShape getOcclusionShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos) {
+        if (blockState.getValue(AGE) == 3) {
+            //return an empty shape to prevent occlusion
+            return Shapes.empty();
+        }
+
+        return super.getOcclusionShape(blockState, blockGetter, blockPos);
     }
 
     @Override

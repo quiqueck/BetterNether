e73299d77cf0a5c30f5652b7a073c63297e906d7
Frank <developer@ambertation.de>
2023-12-19 11:12:30 +0100

    Fixed Loot Accessors

--- src/main/java/org/betterx/betternether/mixin/common/LootTableBuilderAccessor.java
+++ src/main/java/org/betterx/betternether/mixin/common/LootTableBuilderAccessor.java
@@ -4,6 +4,8 @@ import net.minecraft.world.level.storage.loot.LootPool;
 import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.functions.LootItemFunction;
 
+import com.google.common.collect.ImmutableList;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
@@ -12,8 +14,8 @@ import java.util.List;
 @Mixin(LootTable.Builder.class)
 public interface LootTableBuilderAccessor {
     @Accessor("pools")
-    List<LootPool> getPools();
+    ImmutableList.Builder<LootPool> getPools();
 
     @Accessor("functions")
-    List<LootItemFunction> getFunctions();
+    ImmutableList.Builder<LootItemFunction> getFunctions();
 }

083a68a58205b82928441648354e717ca2a08bc1
Frank <developer@ambertation.de>
2023-12-29 02:37:12 +0100

    Adopted Name Changes

--- src/main/java/org/betterx/betternether/config/WorldConfig.java
+++ src/main/java/org/betterx/betternether/config/WorldConfig.java
@@ -2,17 +2,15 @@ package org.betterx.betternether.config;
 
 import de.ambertation.wunderlib.configs.ConfigFile;
 import org.betterx.betternether.BetterNether;
-import org.betterx.wover.entrypoint.WoverCore;
+import org.betterx.wover.config.api.MainConfig;
 
 public class WorldConfig extends ConfigFile {
-    public final static Group STRUCTURE_GROUP = new Group(WoverCore.C.namespace, "structure", 0);
-
     public final BooleanValue addNetherCityToOverworld = new BooleanValue(
-            STRUCTURE_GROUP.title(),
+            MainConfig.STRUCTURE_GROUP.title(),
             "city_in_overworld",
             true
-    ).setGroup(STRUCTURE_GROUP);
-    
+    ).setGroup(MainConfig.STRUCTURE_GROUP);
+
     public WorldConfig() {
         super(BetterNether.C, "world");
     }

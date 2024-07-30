94b78fcd4baa38074c7c6debb60f2646934040f5
Frank Bauer <frank.bauer@fau.de>
2024-07-30 18:06:02 +0200

    [Fix] Use changed DataComponent API

--- src/main/java/org/betterx/betternether/registry/NetherEnchantments.java
+++ src/main/java/org/betterx/betternether/registry/NetherEnchantments.java
@@ -1,6 +1,7 @@
 package org.betterx.betternether.registry;
 
 import org.betterx.betternether.BetterNether;
+import org.betterx.wover.data_components.DataComponentManager;
 import org.betterx.wover.enchantment.api.EnchantmentKey;
 import org.betterx.wover.enchantment.api.EnchantmentManager;
 
@@ -15,7 +16,7 @@ public class NetherEnchantments {
     ;
     public static EnchantmentKey RUBY_FIRE = EnchantmentManager.createKey(BetterNether.C.id("ruby_fire"));
 
-    public static final Holder<Attribute> OBSIDIAN_BLOCK_BREAK_SPEED = EnchantmentManager.registerAttribute(
+    public static final Holder<Attribute> OBSIDIAN_BLOCK_BREAK_SPEED = DataComponentManager.registerAttribute(
             BetterNether.C.mk("player.obsidian_block_break_speed"),
             new RangedAttribute("attribute.name.player.bn_obsidian_block_break_speed", 1.0, 1.0f, 100.0f).setSyncable(true)
     );
